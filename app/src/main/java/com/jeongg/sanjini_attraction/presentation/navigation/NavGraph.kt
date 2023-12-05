8203bfdcc82b4d79e4c29556814f9cdcfa9cbc9e
WoeunJung <leena09123@naver.com>
2023-12-05 18:32:03 +0900

    feat: game associated navigation

--- app/src/main/java/com/jeongg/sanjini_attraction/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/jeongg/sanjini_attraction/presentation/navigation/NavGraph.kt
@@ -2,16 +2,37 @@ package com.jeongg.sanjini_attraction.presentation.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.jeongg.sanjini_attraction.presentation.view.bluetooth.BluetoothScreen
+import com.jeongg.sanjini_attraction.presentation.view.finish_game.FinishGameScreen
+import com.jeongg.sanjini_attraction.presentation.view.game.GameScreen
 import com.jeongg.sanjini_attraction.presentation.view.ready_game.ReadyForGameScreen
 import com.jeongg.sanjini_attraction.presentation.view.splash.SplashScreen
 
 fun NavGraphBuilder.sanjiniAttractionGraph(
     navController: NavController
 ){
-    composable(route = Screen.SplashScreen.route){ SplashScreen(navController) }
-    composable(route = Screen.BluetoothScreen.route) { BluetoothScreen(navController) }
-    composable(route = Screen.ReadyForGameScreen.route) { ReadyForGameScreen(navController) }
-
+    composable(route = Screen.SplashScreen.route){
+        SplashScreen(navController)
+    }
+    composable(route = Screen.BluetoothScreen.route) {
+        BluetoothScreen(navController)
+    }
+    composable(route = Screen.ReadyForGameScreen.route) {
+        ReadyForGameScreen(navController)
+    }
+    composable(
+        route = Screen.GameScreen.route + "?index={index}",
+        arguments = listOf(
+            navArgument("index"){
+                type = NavType.IntType
+                defaultValue = 0
+            }
+        )
+    ){
+        GameScreen(navController)
+    }
+    composable(route = Screen.FinishGameScreen.route){FinishGameScreen(navController)}
 }
--- app/src/main/java/com/jeongg/sanjini_attraction/presentation/navigation/Screen.kt
+++ app/src/main/java/com/jeongg/sanjini_attraction/presentation/navigation/Screen.kt
@@ -4,4 +4,6 @@ sealed class Screen (val route: String){
     object SplashScreen: Screen("splash_screen")
     object BluetoothScreen: Screen("bluetooth_screen")
     object ReadyForGameScreen: Screen("ready_for_game_screen")
+    object GameScreen: Screen("game_screen")
+    object FinishGameScreen: Screen("finish_game_screen")
 }
